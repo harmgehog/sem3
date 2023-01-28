473eb29f5cede63026895108289227563cc16c74
harmgehog <polstia@gmail.com>
2023-01-28 13:40:36 +0300

    remark 3

---
+++ task17/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void CheckQarter(int x, int y)
+{
+    if (x > 0 & y > 0)
+    {
+        Console.WriteLine(1);
+    }
+    if (x < 0 & y > 0)
+    {
+        Console.WriteLine(2);
+    }
+    if (x < 0 & y < 0)
+    {
+        Console.WriteLine(3);
+    }
+    if (x > 0 & y < 0)
+    {
+        Console.WriteLine(4);
+    }
+
+}
+
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+
+// ввод данных от пользователя
+
+int x = GetNum("Input value X");
+int y = GetNum("Input value Y");
+Console.Write("qarter is: ");
+
+// find quarter
+CheckQarter(x, y);
---
+++ task18/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+
+void CheckQr(int x)
+{
+    if (x == 1)
+    {
+        Console.WriteLine("Where: x > 0 and y > 0");
+    }
+    if (x == 2)
+    {
+        Console.WriteLine("Where: x < 0 & y > 0");
+    }
+    if (x == 3)
+    {
+        Console.WriteLine("Where: x < 0 & y < 0");
+    }
+    if (x == 4)
+    {
+        Console.WriteLine("Where: x > 0 & y < 0");
+    }
+
+}
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+// ввод данных от пользователя
+
+int x = GetNum("Input qarter: ");
+Console.Write("area is: ");
+
+// find quarter
+CheckQr(x);
